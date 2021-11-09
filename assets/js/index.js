df23d602d8933946a8a1a689f21d19f1c083a0fb
Ben Border <ben@bunnbuns.net>
2021-11-08 23:54:49 -0500

    poop

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+function toBlock(id, data) {
+    console.log(post)
+    var post = document.createElement("div")
+    post.setAttribute("class", "post")
+    var top = document.createElement("div")
+    top.appendChild(document.createTextNode(data['title']))
+    post.appendChild(top)
+    return post
+}
+
+fetch("https://api.stibarc.com/v2/getposts.sjs").then(response => response.json()).then((json) => {
+    $("recent-posts").innerHTML = ""
+    var keys = Object.keys(json)
+    lastloaded = keys[0]
+    var posts = document.createElement("div")
+    posts.setAttribute("class", "posts")
+    for (var i = 19; i >= 0; i--) {
+        posts.appendChild(toBlock(keys[i], json[keys[i]]))
+    }
+    $("recent-posts").appendChild(posts)
+}).catch((err) => {})
