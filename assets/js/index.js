d7faf5d3b76d2ec1e2078a3564623913f99c4039
Ben Border <ben@bunnbuns.net>
2021-11-21 01:18:56 -0500

    feed selet working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,98 @@
-var lastloaded = 0;
-var lastfollowloaded = 0;
+var lastloaded = 0
+var lastfollowloaded = 0
+var feedState = "global"
 
-function loadMoreBlock() {
+$("global-btn").onclick = function () {
+    feedSelect("global")
+}
+
+$("followed-btn").onclick = function () {
+    feedSelect("followed")
+}
+
+function feedSelect(state) {
+    if (state == "followed") {
+        feedState = "followed"
+        $("global-btn").classList.remove("primary")
+        $("followed-btn").classList.add("primary")
+        $("global-posts").style.display = "none"
+        $("global-load-more").style.display = "none"
+        $("followed-posts").style.display = ""
+        $("followed-load-more").style.display = ""
+    } else {
+        feedState = "global"
+        $("followed-btn").classList.remove("primary")
+        $("global-btn").classList.add("primary")
+        $("followed-posts").style.display = "none"
+        $("followed-load-more").style.display = "none"
+        $("global-posts").style.display = ""
+        $("global-load-more").style.display = ""
+    }
+}
+
+
+function loadMoreBlock(id) {
     var loadMore = document.createElement("button")
     loadMore.setAttribute("class", "button full")
-    loadMore.setAttribute("onclick", "loadMore()")
+    loadMore.setAttribute("id", id)
+    loadMore.setAttribute("onclick", `loadMore("${id}")`)
     loadMore.appendChild(document.createTextNode("Load More"))
     return loadMore
 }
 
-function loadMore() {
-    fetch("https://api.stibarc.com/v2/getposts.sjs?id=" + lastloaded).then(response => response.json()).then((json) => {
-        var posts = "";
-        for (var i = lastloaded - 1; i > lastloaded - 21; i--) {
-            posts += toBlock(i, json[i])
+function loadMore(id) {
+    if (id == "followed-load-more") {
+        fetch(`${rootAPI}/v3/getfollowposts.sjs?sess=${sess}&id=${lastfollowloaded}`).then(response => response.json()).then((json) => {
+            lastloaded = lastloaded - 20
+            var keys = Object.keys(json)
+            var posts = ""
+            for (var i = keys.length-1; i >= 0; i--) {
+                posts += toBlock(keys[i], json[keys[i]])
+                lastfollowloaded = keys[i]
+            }
+            lastfollowloaded = keys[0]
+            $("followed-posts").innerHTML += posts
+        }).catch((err) => { console.log(err) })
+    } else {
+        fetch(`${rootAPI}/v2/getposts.sjs?id=${lastloaded}`).then(response => response.json()).then((json) => {
+            var posts = "";
+            for (var i = lastloaded - 1; i > lastloaded - 21; i--) {
+                posts += toBlock(i, json[i])
+            }
+            $("global-posts").innerHTML += posts
+            lastloaded = lastloaded - 20;
+        }).catch((err) => { console.log(err) })
+    }
+
+}
+
+function loadPosts() {
+    fetch(`${rootAPI}/v2/getposts.sjs`).then(response => response.json()).then((json) => {
+        $("global-posts").innerHTML = ""
+        var keys = Object.keys(json)
+        lastloaded = keys[0]
+        var posts = ""
+        for (var i = 19; i >= 0; i--) {
+            posts += toBlock(keys[i], json[keys[i]])
         }
-        $("recent-posts").innerHTML += posts
-        lastloaded = lastloaded - 20;
+        $("global-posts").innerHTML = posts
+        $("loadMoreContainer").appendChild(loadMoreBlock("global-load-more"))
+        feedSelect(feedState)
+    }).catch((err) => { console.log(err) })
+
+    fetch(`${rootAPI}/v3/getfollowposts.sjs?sess=${sess}`).then(response => response.json()).then((json) => {
+        $("followed-posts").innerHTML = ""
+        var keys = Object.keys(json)
+        var posts = ""
+        for (var i = keys.length - 1; i >= 0; i--) {
+            posts += toBlock(keys[i], json[keys[i]])
+            lastfollowloaded = keys[i]
+        }
+        lastfollowloaded = keys[0]
+        $("followed-posts").innerHTML = posts
+        $("loadMoreContainer").appendChild(loadMoreBlock("followed-load-more"))
+        feedSelect(feedState)
     }).catch((err) => { console.log(err) })
 }
 
-fetch(rootAPI + "/v2/getposts.sjs").then(response => response.json()).then((json) => {
-    $("recent-posts").innerHTML = ""
-    var keys = Object.keys(json)
-    lastloaded = keys[0]
-    var posts = ""
-    for (var i = 19; i >= 0; i--) {
-        posts += toBlock(keys[i], json[keys[i]])
-    }
-    $("recent-posts").innerHTML = posts
-    $("loadMoreContainer").appendChild(loadMoreBlock());
-}).catch((err) => { console.log(err) })
+loadPosts()
